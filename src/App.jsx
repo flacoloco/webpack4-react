ad280ba1260eb2a54a7d23b149b3e64ad42c7f2f
Alejandro Vásquez <alejandro.vasquez@bigfinite.com>
2019-06-17 23:34:47 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Hello = styled.p`
+  background-color: pink;
+  padding: 20px;
+`
+
+const App = () => (
+  <Hello>React here!</Hello>
+)
+
+export default App
